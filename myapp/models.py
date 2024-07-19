803fd2c0292ee0623779616da33dd3ed6ded5058
Anjaldev-vk <mkmanjal@gmail.com>
2024-07-19 15:10:16 +0530

    Your commit message here

---
+++ myapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Item(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    image=models.ImageField(upload_to='images/')
+    def _str_(self):
+        return self.name
