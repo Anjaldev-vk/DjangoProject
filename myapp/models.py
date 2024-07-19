4ab8c1802a22b3553f6be42e989bf4fedc4da33b
Anjaldev-vk <mkmanjal@gmail.com>
2024-07-19 16:03:08 +0530

    second commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Item(models.Model):
-    name=models.CharField(max_length=100)
-    description=models.TextField()
-    image=models.ImageField(upload_to='images/')
-    def _str_(self):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Decimal field for price
+    image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
         return self.name
